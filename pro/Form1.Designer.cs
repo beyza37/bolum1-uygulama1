00d938859c623fdf17b72fdc058237eae7ec7e44
beyza37 <145428578+beyza37@users.noreply.github.com>
2023-09-19 11:14:45 +0300

    Add files via upload
    Label invite

--- pro/Form1.Designer.cs
+++ pro/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Beyza = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Beyza
+            // 
+            this.Beyza.AutoSize = true;
+            this.Beyza.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Beyza.Location = new System.Drawing.Point(51, 159);
+            this.Beyza.Name = "Beyza";
+            this.Beyza.Size = new System.Drawing.Size(451, 69);
+            this.Beyza.TabIndex = 0;
+            this.Beyza.Text = "Beyza Gözüpek";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(51, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Beyza Gözüpek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Beyza);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Beyza;
+        private System.Windows.Forms.Label label1;
     }
 }
 
